54691bd415db0b79b29520bbfde3e42d1841966a
Aaron Peyton <thealphabet101@gmail.co>
2017-10-11 16:37:45 -0700

    still dunno...

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route("/add")
 def render_main():
