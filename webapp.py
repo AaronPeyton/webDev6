ac0685a4500892e80ec343e9c8d7b607b3ce438b
Aaron Peyton <thealphabet101@gmail.co>
2017-10-12 09:59:19 -0700

    troubleshot

--- webapp.py
+++ webapp.py
@@ -28,16 +28,10 @@ def render_response():
             reply = float(request.args['add1']) + float(request.args['add2'])
         else:
             reply = "null"
-    elif 'mult1' in request.args and 'mult2' in request.args
-        if request.args['mult1'] != null and  request.args['mult2'] != null:
-            reply = float(request.args['mult1']) * float(request.args['mult2'])
-        else:
-            reply = "null"
+    elif 'mult1' in request.args and 'mult2' in request.args:
+        reply = float(request.args['mult1']) * float(request.args['mult2'])
     elif 'hex' in request.args:
-        if request.args['hex'] != null:
-            reply = format(int(request.args['hex']),'x')
-        else:
-            reply = "null"
+        reply = format(int(request.args['hex']),'x')
     else:
       reply = "i really dunno dude"
     # reply = "ahhhhhh. work!"
